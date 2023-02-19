5492fabff1cfa8ce1c3d392b8b1053ea66a061c6
zispo <marcosguilhermefh@gmailcom>
2023-02-18 23:21:05 -0300

    send

--- src/main/java/xyz/zapgrupos/Main.java
+++ src/main/java/xyz/zapgrupos/Main.java
@@ -10,13 +10,13 @@ public class Main {
         System.out.println("b: verifyGroup?");
         System.out.println("c: sensible");
         String commander;
-        if(args != null){
+
+        try{
             commander = args[0];
-        }else{
+        }catch (ArrayIndexOutOfBoundsException e){
             Scanner s = new Scanner(System.in);
             commander = s.next();
         }
-
 
         switch (commander){
             case "a":
