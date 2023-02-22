3cac93e6720fa1ad80a6ca5c43935e802d5f3622
zispo <marcosguilhermefh@gmailcom>
2023-02-22 01:12:28 -0300

    ajuste

--- src/main/java/xyz/zapgrupos/application/scrap/VerifyIfActive.java
+++ src/main/java/xyz/zapgrupos/application/scrap/VerifyIfActive.java
@@ -18,7 +18,7 @@ public class VerifyIfActive<T extends Grupo>  implements ScrapContract<T,Boolean
     }
 
     private boolean verificar(Telegram grupo){
-        if( (grupo.getTitulo().isEmpty()) || (grupo.getTitulo().isBlank()) || (grupo.getTitulo() == null)){
+        if( (grupo.getTitulo().isEmpty()) | (grupo.getTitulo().isBlank()) | !(grupo.getTitulo() == null)){
             grupo.setAtivo(true);
             System.out.println(grupo.getImg());
             System.out.println(String.format("O grupos %s para telegram está ativo", grupo.getTitulo()));
