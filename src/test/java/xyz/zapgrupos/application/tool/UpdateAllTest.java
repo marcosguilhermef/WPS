38b6ee5bfb9e35abe5a4455d8285ff1be2d35047
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-02-24 12:55:57 -0300

    tentativa de redução de memoria ran

--- src/test/java/xyz/zapgrupos/application/tool/UpdateAllTest.java
+++ src/test/java/xyz/zapgrupos/application/tool/UpdateAllTest.java
@@ -7,6 +7,6 @@ public class UpdateAllTest {
     @Test
     public void updateAll(){
         Commander t = new Commander();
-        t.run(new UpdateAll(), "6142752e1dbe863be63f09f8");
+        t.run(new UpdateAll());
     }
 }
